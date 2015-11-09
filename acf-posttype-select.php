930810e5d8e31f15e09b4cc868d342566f4b5548
Glenn Welser <glenn.welser@gmail.com>
2015-11-09 08:00:35 -0500

    initial commit after rename

--- acf-posttype-select.php
+++ acf-posttype-select.php
@@ -11,17 +11,9 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-
-
-
-// 1. set text domain
 // Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
 load_plugin_textdomain('acf-posttype_select', false, dirname(plugin_basename(__FILE__)) . '/lang/');
 
-
-
-
-// 2. Include field type for ACF5
 // $version = 5 and can be ignored until ACF6 exists
 function include_field_types_posttype_select($version)
 {
@@ -30,10 +22,7 @@ function include_field_types_posttype_select($version)
 
 add_action('acf/include_field_types', 'include_field_types_posttype_select');
 
-
-
-
-// 3. Include field type for ACF4
+// Include field type for ACF4
 function register_fields_posttype_select()
 {
     include_once('acf-posttype-select-v4.php');
